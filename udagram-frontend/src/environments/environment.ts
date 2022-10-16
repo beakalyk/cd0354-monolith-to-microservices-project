0cae514372c71bdb8e4a44e7cade1c141755c915
beakalyk <akaabe@hotmail.com>
2022-10-17 01:37:08 +0300

    new buld for the frontbuld

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa079fec92bd445ec892734a3ff2effa-1693444857.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
